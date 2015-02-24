a925ce8ddb72a533d05e9141cc508b9d6e11522b
顾翔 <c0710204@gmail.com>
2015-02-24 21:39:01 +0800

    homepage

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <script src="http://cdn.bootcss.com/bootstrap/3.3.2/js/bootstrap.min.js">
+    </script>
+    
+    <body>
+        <div class="jumbotron">
+            <h1>
+                This is a api host,no web page service provide!
+            </h1>
+            <p>
+                System Time:<?php date_default_timezone_set("Asia/Hong_Kong") ; echo date("Y-m-d H:i:s");?>
+            </p>
+            <p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">
+                    Learn NOTHING
+                </a>
+            </p>
+        </div>
+        <div class="container">
+        </div>
+    </body>
+</html>
